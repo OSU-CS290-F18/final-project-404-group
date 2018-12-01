2a7fc61c8c1f9c36aaf4835786e6e3361c58da14
ink19 <nkzxwgr@163.com>
2018-12-01 11:00:50 +0800

    Optimize dirs, add a fixed button

--- server.js
+++ server.js
@@ -42,14 +42,10 @@ app.get('/find', function(req, res){
     })
 })
 
-app.get('/*.(html|css|js)', function(req, res) {
-    fs.readFile("."+req.path, function(err, data){
-        res.send(data.toString())
-    })
-})
+app.use(express.static('./static'))
 
 app.get('/', function(req, res) {
-    fs.readFile("./index.html", function(err, data){
+    fs.readFile("./static/index.html", function(err, data){
         res.send(data.toString())
     })
 })
