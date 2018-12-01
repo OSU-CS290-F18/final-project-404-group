cf9461313eae32de079672429fda128b965b22eb
ink19 <nkzxwgr@163.com>
2018-12-01 21:42:36 +0800

    add add post button

--- static/main.js
+++ static/main.js
@@ -1,13 +1,15 @@
 $(function(){
 
 var vueData = {
-    now_group : "",
-    now_post : "",
+    now_group : {},
+    now_post : {},
     mainWindow: "main-comp"
 };
 
 var vueMethods = {
-
+    switch_component: function(component_name) {
+        vueData.mainWindow = component_name;
+    }
 };
 
 Vue.component('main-comp', {
@@ -15,7 +17,8 @@ Vue.component('main-comp', {
     data: function() {
         return {
             input_topic: "",
-            input_summary: ""
+            input_summary: "",
+            group_list: []
         };
     },
     methods: {
@@ -27,10 +30,40 @@ Vue.component('main-comp', {
             }, function(res) {
                 alert(res);
             });
+        },
+        get_into_group: function(id) {
+            vueData.now_group = this.$data.group_list[id];
+            vueMethods.switch_component("group-comp");
         }
     },
     mounted: function() {
+        __this = this;
+        $.get('./get_group_list', function(res) {
+            if(res['code'] == 0) {
+                __this.$data.group_list = __this.$data.group_list.concat(res['data']);
+            } else {
+                alert(res['err_msg']);
+            }
+        })
+    }
+});
 
+Vue.component('group-comp', {
+    template: "#group-temp",
+    data: function() {
+        return {
+            topic: "",
+            summary: ""
+        };
+    },
+    methods: {
+        get_into_home: function() {
+            vueMethods.switch_component("main-comp");
+        }
+    },
+    mounted: function() {
+        this.$data.topic = vueData.now_group.topic;
+        this.$data.summary = vueData.now_group.summary;
     }
 });
 
