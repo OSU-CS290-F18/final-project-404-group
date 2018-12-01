b512bd97bde7183211ea45422e20ffdeafa47f55
ink19 <nkzxwgr@163.com>
2018-12-02 00:25:57 +0800

    Group function over

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 
 app.post('/add_group', function(req, res) {
+    res.append('Content-Type', 'application/json')
     MongoClient.connect(mongodb_url, function(err, client) {
         if(err) {
             console.log(err)
@@ -30,12 +31,12 @@ app.post('/add_group', function(req, res) {
             }, function(err, result){
                 if(err) {
                     res.send(JSON.stringify({
-                        code: '-1',
+                        code: -1,
                         err_msg: 'insert data fail'
                     }))
                 } else {
                     res.send(JSON.stringify({
-                        code: '0',
+                        code: 0,
                         data: result.insertedId
                     }))
                 }
@@ -45,27 +46,141 @@ app.post('/add_group', function(req, res) {
     });
 })
 
-app.get('/get_group_list', function(req, res) {
+app.post('/add_post', function(req, res) {
+    res.append('Content-Type', 'application/json')
+    MongoClient.connect(mongodb_url, function(err, client) {
+        if(err) {
+            console.log(err)
+            res.send(JSON.stringify({
+                'code' : -1,
+                'err_msg' : 'open database fail'
+            }))
+        } else {
+            var db = client.db("animals")
+            db.collection('post').insertOne({
+                title: req.body.title,
+                author: req.body.author,
+                content: req.body.content,
+                commit_number: 0,
+                group_id: req.body.group_id,
+                update_time: Date.now(),
+                create_time: Date.now()
+            }, function(err, result){
+                if(err) {
+                    res.send(JSON.stringify({
+                        code: -1,
+                        err_msg: 'insert data fail'
+                    }))
+                } else {
+                    res.send(JSON.stringify({
+                        code: 0,
+                        data: result.insertedId
+                    }))
+                    
+                }
+            })
+            // db.collection('group').updateOne({
+            //     _id: req.body.group_id
+            // }, {
+            //     $set: {
+            //         update_time: Date.now()
+            //     }
+            // }, function(err, update_res){
+            //     console.log(update_res)
+            //     client.close()
+            // })
+        }
+    });
+})
+
+app.post('/get_post_list', function(req, res) {
+    res.append('Content-Type', 'application/json')
     MongoClient.connect(mongodb_url, function (err, client) {
         if (err) {
             console.log(err)
             res.send(JSON.stringify({
-                code : '-1',
+                code : -1,
                 err_msg : 'connect fails'
             }))
         } else {
             var db = client.db('animals')
-            db.collection('group').find({}).toArray(function (err, result) {
+            db.collection('post').find({
+                group_id: req.body.group_id
+            }).sort([
+                ['update_time', -1]
+            ]).toArray(function (err, result) {
                 if(err) {
                     res.send(JSON.stringify({
-                        code: '-1',
+                        code: -1,
                         err_msg: 'find data fail'
                     }))
                 } else {
-                    res.send({
-                        code: '0',
+                    res.send(JSON.stringify({
+                        code: 0,
                         data: result
-                    })
+                    }))
+                }
+                client.close()
+            })
+        }
+    })
+})
+
+app.get('/get_group_list', function(req, res) {
+    res.append('Content-Type', 'application/json')
+    MongoClient.connect(mongodb_url, function (err, client) {
+        if (err) {
+            console.log(err)
+            res.send(JSON.stringify({
+                code : -1,
+                err_msg : 'connect fails'
+            }))
+        } else {
+            var db = client.db('animals')
+            db.collection('group').find({}).sort([['update_time', -1]]).toArray(function (err, result) {
+                if(err) {
+                    res.send(JSON.stringify({
+                        code: -1,
+                        err_msg: err
+                    }))
+                } else {
+                    res.send(JSON.stringify({
+                        code: 0,
+                        data: result
+                    }))
+                }
+                client.close()
+            })
+        }
+    })
+})
+
+app.post('/get_post_list_limit', function(req, res) {
+    res.append('Content-Type', 'application/json')
+    MongoClient.connect(mongodb_url, function (err, client) {
+        if (err) {
+            console.log(err)
+            res.send(JSON.stringify({
+                code : -1,
+                err_msg : 'connect fails'
+            }))
+        } else {
+            var db = client.db('animals')
+            db.collection('post').find({
+                group_id: req.body.group_id
+            }).sort([
+                ['update_time', -1]
+            ]).limit(5).toArray(function (err, result) {
+                if(err) {
+                    res.send(JSON.stringify({
+                        code: -1,
+                        err_msg: 'find data fail'
+                    }))
+                } else {
+                    res.send(JSON.stringify({
+                        code: 0,
+                        data: result
+                    }))
                 }
                 client.close()
             })
--- static/main.js
+++ static/main.js
@@ -24,11 +24,20 @@ Vue.component('main-comp', {
     methods: {
         add_group: function() {
             //alert(this.$data.input_topic + this.$data.input_summary);
+            __this = this;
             $.post('./add_group', {
                 topic: this.$data.input_topic,
                 summary: this.$data.input_summary
             }, function(res) {
-                alert(res);
+                if(0 == res['code']) {
+                    __this.$data.group_list.unshift({
+                        _id: res['data'],
+                        topic: __this.$data.input_topic,
+                        summary: __this.$data.input_summary
+                    });
+                } else {
+                    alert(res['err_msg']);
+                }
             });
         },
         get_into_group: function(id) {
@@ -40,6 +49,19 @@ Vue.component('main-comp', {
         __this = this;
         $.get('./get_group_list', function(res) {
             if(res['code'] == 0) {
+                res['data'].map(function(value, index, ar) {
+                    value['post_list'] = [];
+                    $.post('./get_post_list_limit', {
+                        group_id: value._id
+                    }, function(res2) {
+                        if(res2['code'] == 0) {
+                            ar[index]['post_list'] = ar[index]['post_list'].concat(res2['data'])
+                        } else {
+                            alert(res2['err_msg'])
+                        }
+                    });
+                    return value;
+                })
                 __this.$data.group_list = __this.$data.group_list.concat(res['data']);
             } else {
                 alert(res['err_msg']);
@@ -53,17 +75,69 @@ Vue.component('group-comp', {
     data: function() {
         return {
             topic: "",
-            summary: ""
+            summary: "",
+            input_title: "",
+            input_content: "",
+            input_author: "",
+            post_list: []
         };
     },
     methods: {
         get_into_home: function() {
             vueMethods.switch_component("main-comp");
+        },
+        add_post: function() {
+            __this = this;
+            $.post('./add_post', {
+                title: this.$data.input_title,
+                content: this.$data.input_content,
+                author: this.$data.input_author,
+                group_id: vueData.now_group._id
+            }, function(res) {
+                if(res['code'] == 0) {
+                    __this.$data.post_list.unshift({
+                        title: __this.$data.input_title,
+                        author: __this.$data.input_author,
+                        content: __this.$data.input_content,
+                        commit_number: 0,
+                        group_id: vueData.now_group._id,
+                        update_time: Date.now(),
+                        create_time: Date.now()
+                    })
+                } else {
+                    alert(res['err_msg'])
+                }
+            })
         }
     },
     mounted: function() {
+        __this = this;
         this.$data.topic = vueData.now_group.topic;
         this.$data.summary = vueData.now_group.summary;
+        $.post('./get_post_list', {
+            group_id: vueData.now_group._id
+        },function(res) {
+            if(res['code'] == 0) {
+                __this.$data.post_list = __this.$data.post_list.concat(res['data']);
+            } else {
+                alert(res['err_msg']);
+            }
+        })
+    }
+});
+
+Vue.component('post-comp', {
+    template: "#post-temp",
+    data: function() {
+        return {
+
+        };
+    },
+    methods: {
+
+    },
+    mounted: function() {
+        
     }
 });
 
