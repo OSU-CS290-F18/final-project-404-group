790a85392ff6be736bd62875a905eda6ad14dd2a
ink19 <nkzxwgr@163.com>
2018-12-01 21:20:42 +0800

    add clientjs, dadta

--- server.js
+++ server.js
@@ -3,10 +3,75 @@ const app = express()
 const mongodb = require('mongodb')
 const fs = require('fs')
 const mongodb_url = 'mongodb://mymongo:27017/animals'
+const bodyParser = require('body-parser')
+
 
 var MongoClient = mongodb.MongoClient
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
+
+app.post('/add_group', function(req, res) {
+    MongoClient.connect(mongodb_url, function(err, client) {
+        if(err) {
+            console.log(err)
+            res.send(JSON.stringify({
+                'code' : -1,
+                'err_msg' : 'open database fail'
+            }))
+        } else {
+            var db = client.db("animals")
+            db.collection('group').insertOne({
+                topic: req.body.topic,
+                summary: req.body.summary,
+                update_time: Date.now(),
+                create_time: Date.now()
+            }, function(err, result){
+                if(err) {
+                    res.send(JSON.stringify({
+                        code: '-1',
+                        err_msg: 'insert data fail'
+                    }))
+                } else {
+                    res.send(JSON.stringify({
+                        code: '0',
+                        data: result.insertedId
+                    }))
+                }
+                client.close()
+            })
+        }
+    });
+})
+
+app.get('/get_group_list', function(req, res) {
+    MongoClient.connect(mongodb_url, function (err, client) {
+        if (err) {
+            console.log(err)
+            res.send(JSON.stringify({
+                code : '-1',
+                err_msg : 'connect fails'
+            }))
+        } else {
+            var db = client.db('animals')
+            db.collection('group').find({}).toArray(function (err, result) {
+                if(err) {
+                    res.send(JSON.stringify({
+                        code: '-1',
+                        err_msg: 'find data fail'
+                    }))
+                } else {
+                    res.send({
+                        code: '0',
+                        data: result
+                    })
+                }
+                client.close()
+            })
+        }
+    })
+})
 
 app.get('/add', function(req, res){
     MongoClient.connect(mongodb_url, function (err, client) {
--- static/main.js
+++ static/main.js
@@ -1,11 +1,38 @@
 $(function(){
+
 var vueData = {
-    
+    now_group : "",
+    now_post : "",
+    mainWindow: "main-comp"
 };
 
 var vueMethods = {
 
 };
+
+Vue.component('main-comp', {
+    template: '#main-temp',
+    data: function() {
+        return {
+            input_topic: "",
+            input_summary: ""
+        };
+    },
+    methods: {
+        add_group: function() {
+            //alert(this.$data.input_topic + this.$data.input_summary);
+            $.post('./add_group', {
+                topic: this.$data.input_topic,
+                summary: this.$data.input_summary
+            }, function(res) {
+                alert(res);
+            });
+        }
+    },
+    mounted: function() {
+
+    }
+});
 
 var vm = new Vue({
     el: '#mainView',
